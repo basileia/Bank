ec60b47d40fdacb7f0e69119ebb110a9cad578ca
basileia <jane.novak@centrum.cz>
2021-04-10 15:27:04 +0200

    Add project files.

---
+++ Account.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank
+{
+    class Account : IAccount
+    {
+        public double Balance { get; set; }
+
+        public string Owner { get; private set; }
+
+        public Account(double openingBalance, string owner)
+        {
+            Balance = openingBalance;
+            Owner = owner;
+        }
+    }
+}
---
+++ IAccount.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank
+{
+    interface IAccount
+    {
+        double Balance { get; }
+        string Owner { get; }
+    }
+}
